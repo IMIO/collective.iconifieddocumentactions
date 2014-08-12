85c9488337a148d5996a5530ec454ceffb582d68
Gauthier Bastien <g.bastien@imio.be>
2014-08-12 14:03:55 +0200

    Adapted upgrade step to remove old registered CSS from portal_css

--- communesplone/iconified_document_actions/upgrades.py
+++ communesplone/iconified_document_actions/upgrades.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
 
 
-def remove_skins_directory(context):
+def cleanup_skins_and_css(context):
     context.runImportStepFromProfile(
-        'communesplone.iconified_document_actions:default', 'cssregistry')
-    portal = context.getSite()
-    portal.portal_skins.manage_delObjects(ids=['iconified_document_actions_styles', ])
+        'profile-communesplone.iconified_document_actions:default', 'cssregistry')
+    portal = getToolByName(context, 'portal_url').getPortalObject()
+    # remove old registered skin
+    if hasattr(portal.portal_skins, 'iconified_document_actions_styles'):
+        portal.portal_skins.manage_delObjects(ids=['iconified_document_actions_styles', ])
+    # remove old registered CSS, does not fail if resource not exists
+    portal.portal_css.unregisterResource('iconified_document_actions.css')
