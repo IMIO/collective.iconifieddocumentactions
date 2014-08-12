48a943610477c4093d9d4927f6c92c06513beda0
Benoît Suttor <benoit.suttor@imio.be>
2014-08-12 13:48:24 +0200

    remove skins.xml, remove skin folder from python upgrade step

--- communesplone/iconified_document_actions/upgrades.py
+++ communesplone/iconified_document_actions/upgrades.py
@@ -2,5 +2,7 @@
 
 
 def remove_skins_directory(context):
-    context.runAllImportStepsFromProfile(
-        'communesplone.iconified_document_actions:default')
+    context.runImportStepsFromProfile(
+        'communesplone.iconified_document_actions:default', 'cssregistry')
+    portal = context.getSite()
+    portal.portal_skins.manage_delObjects(ids=['iconified_document_actions_styles', ])
