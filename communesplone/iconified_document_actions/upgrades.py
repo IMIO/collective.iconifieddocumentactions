06e498ce44f086ccc7bd8a46ab36eaa066c77441
Benoît Suttor <benoit.suttor@imio.be>
2014-08-12 13:49:56 +0200

    typo error

--- communesplone/iconified_document_actions/upgrades.py
+++ communesplone/iconified_document_actions/upgrades.py
@@ -2,7 +2,7 @@
 
 
 def remove_skins_directory(context):
-    context.runImportStepsFromProfile(
+    context.runImportStepFromProfile(
         'communesplone.iconified_document_actions:default', 'cssregistry')
     portal = context.getSite()
     portal.portal_skins.manage_delObjects(ids=['iconified_document_actions_styles', ])
